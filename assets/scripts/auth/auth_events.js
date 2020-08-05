8f41849ee3426b71246fe92e709ef223eac76667
rubysattar <rubysattar@gmail.com>
2020-08-05 14:29:33 -0400

    started client auth

---
+++ assets/scripts/auth/auth_events.js
@@ -0,0 +1,51 @@
+'use strict'
+const api = require('./auth_api')
+const getFormFields = require('../../../lib/get-form-fields')
+const ui = require('./auth_ui')
+const store = require('../store')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signUp(formData)
+    .then(ui.signUpSuccess)
+    .then($('#sign-up').hide())
+    .catch(ui.signUpFailure)
+}
+const onSignIn = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signIn(formData)
+    .then((userInfo) => {
+      // console.log({response})
+      store.user = userInfo.user
+    })
+    .then(ui.signInSuccess)
+    .catch(error => {
+      const errorMessage = error.responseJSON.message
+      ui.signInFailure(errorMessage)
+    })
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.changePassword(formData)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
---
+++ assets/scripts/auth/auth_ui.js
@@ -0,0 +1,63 @@
+'use strict'
+const store = require('../store')
+
+const signUpSuccess = function () {
+  $('#auth-message').text('Sign-up was successful!')
+  $('#sign-up').hide()
+  $('form').trigger('reset')
+  $('#sign-in').show()
+}
+
+const signUpFailure = function () {
+  $('#auth-message').text('Sign-up was unsuccessful. Try again.')
+  $('#sign-up').show()
+  $('form').trigger('reset')
+}
+
+const signInSuccess = function () {
+  $('#auth-message').text('Sign-in was successful!')
+  $('form').trigger('reset')
+  $('#change-password').show()
+  $('#sign-out').show()
+  $('#unauthenticated').hide()
+  $('#landing-page').hide()
+}
+const signInFailure = function (errorMessage) {
+  if (errorMessage) {
+    $('#auth-message').text(`Sign-in failed. Reason: ${errorMessage}`)
+  } else {
+    $('#auth-message').text('Sign-in failed.')
+  }
+  $('form').trigger('reset')
+}
+const changePasswordFailure = function () {
+  $('#auth-message').text('Password change failed')
+  $('form').trigger('reset')
+}
+const changePasswordSuccess = function () {
+  $('#auth-message').text('Successfully changed your password! You can change it again or scroll down and start playing!')
+  $('form').trigger('reset')
+}
+const signOutSuccess = function () {
+  $('#auth-message').text('You are now signed out!')
+  $('#sign-up').show()
+  $('#sign-in').show()
+  $('#change-password').hide()
+  $('#sign-out').hide()
+  $('#landing-page').show()
+  store.user = null
+}
+const signOutFailure = function () {
+
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}
