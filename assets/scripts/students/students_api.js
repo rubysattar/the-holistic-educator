34cd83beb427754a37b75d0e8ee7fb1989459779
rubysattar <rubysattar@gmail.com>
2020-08-06 12:22:12 -0400

    some api written for students

---
+++ assets/scripts/students/students_api.js
@@ -0,0 +1,32 @@
+'use strict'
+const config = require('../config')
+const store = require('../store')
+
+const getStudents = function () {
+  return $.ajax({
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    url: config.apiUrl + '/students',
+    method: 'GET',
+    data: {
+      user: store.user.id
+    }
+  })
+}
+
+const createStudents = function (formData) {
+  return $.ajax({
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    url: config.apiUrl + '/students',
+    method: 'POST',
+    data: formData
+  })
+}
+
+module.exports = {
+  getStudents,
+  createStudents
+}
